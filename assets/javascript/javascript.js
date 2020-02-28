8a0bcfca680f2769c63dcebfa998665c3914a90a
Hung Le <hungle913@gmail.com>
2020-02-28 10:32:22 -0800

    added placeholder for anime input. also created function to display animeinfo.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
-var anime = ["Naruto", "Radiant", "Haikyu", "Black Clover", "One Piece", "Food Wars", "Dr. Stone", "Demon Slayer", "Fire Force", "World Trigger", "Fairy Tail", "Gintama", "Overlord", "Attack on Titan", "Bleach", "Nanbaka", "Erased", "Gamers", "Given", "Hunter X Hunter"];
+var animes = ["Naruto", "Radiant", "Haikyu", "Black Clover", "One Piece", "Food Wars", "Dr. Stone", "Demon Slayer", "Fire Force", "World Trigger", "Fairy Tail", "Gintama", "Overlord", "Attack on Titan", "Bleach", "Nanbaka", "Erased", "Gamers", "Given", "Hunter X Hunter"];
+
+function displayAnimeInfo() {
+  var anime = $(this).attr("data-name");
+  var queryURL = "api.giphy.com/v1/gifs/search/?q=" + anime + "&api_key=Hmg2J0TCJmvg6ykc36VoNURbReycFRmz&limit=10&rating=g&rating=pg&rating=pg-13";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+    $("#results").empty()
+  });
+}
   
   });
