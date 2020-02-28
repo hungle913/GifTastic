d9dd4baef693be47ca85c1b0cbb3f6320d4850f7
Hung Le <hungle913@gmail.com>
2020-02-28 10:15:41 -0800

    created array of anime

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
+var anime = ["Naruto", "Radiant", "Haikyu", "Black Clover", "One Piece", "Food Wars", "Dr. Stone", "Demon Slayer", "Fire Force", "World Trigger", "Fairy Tail", "Gintama", "Overlord", "Attack on Titan", "Bleach", "Nanbaka", "Erased", "Gamers", "Given", "Hunter X Hunter"];
   
   });
