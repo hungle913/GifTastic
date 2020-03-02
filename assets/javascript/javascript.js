35f6e72a18f04853807242056b1daeb1a48ec440
Hung Le <hungle913@gmail.com>
2020-03-02 09:40:43 -0800

    removed extra ajax coding and fixed order of code for loading.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,17 +3,8 @@ $(document).ready(function(){
 var animes = ["Naruto", "Radiant", "Haikyu", "Black Clover", "One Piece", "Food Wars", "Dr. Stone", "Demon Slayer", "Fire Force", "World Trigger", "Fairy Tail", "Gintama", "Overlord", "Attack on Titan", "Bleach", "Nanbaka", "Erased", "Gamers", "Given", "Hunter X Hunter"];
 
 function displayAnimeInfo() {
-  var anime = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=Hmg2J0TCJmvg6ykc36VoNURbReycFRmz&limit=10&rating=g&rating=pg&rating=pg-13";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response){
-    $("#results").empty()
-  });
 
-
-function renderButtons() {
+  function renderButtons() {
   $("#anime-buttons").empty();
   for (var i = 0; i < animes.length; i++) {
     var a = $("<button>");
@@ -22,17 +13,48 @@ function renderButtons() {
     a.text(animes[i]);
     $("#anime-buttons").append(a);
   }
-}
+  }
 
-$("#find-anime").on("click", function(event){
-  event.preventDefault();
-  var anime = $("#animeSearch").val().trim();
-  animes.push(anime);
   renderButtons();
-});
 
-$(document).on("click", ".anime", displayAnimeInfo);
-renderButtons();
+  $("#find-anime").on("click", function(event){
+  event.preventDefault();
+  var newAnime = $("#animeSearch").val().trim();
+  animes.push(newAnime);
+  renderButtons();
+  });
+
+  $(document).on("click", ".anime", displayAnimeInfo);
+
+
+  $("button").on("click", function() {
+    var anime = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + anime + "&api_key=Hmg2J0TCJmvg6ykc36VoNURbReycFRmz&limit=10&rating=g&rating=pg&rating=pg-13";
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response){
+    var results = response.data;
+
+    for (var i = 0; i <results.length; i++) {
+      var gifDiv = $("<div>");
+
+      var rating = results[i].rating;
+
+      var p = $("<p>").text("Rating: " + rating);
+
+      var animeImage = $("<img>");
+      animeImage.attr("src", results[i].images.fixed_height.url);
+
+      gifDiv.prepend(animeImage);
+      gifDiv.prepend(p);
+
+      $("#results").prepend(gifDiv);
+      }
+
+    });
+
+  });
   
 }
 displayAnimeInfo()
